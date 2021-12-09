d96cbaaedb6e72edeb00dfdb9cd28e2941c76d96
Bruno Gutierrez <bruno.gutierrez96@gmail.com>
2021-12-09 17:44:59 -0500

    Making the echo as variable [Prod]

--- .circleci/bin/check_and_freeze.sh
+++ .circleci/bin/check_and_freeze.sh
@@ -3,9 +3,9 @@ branchName= "none"
 branchName= $(git log -1 --pretty=%B)
 echo $branchName
 PRTag=$(echo $branchName | cut -d '[' -f2 | cut -d ']' -f1)
-echo PRTag
+echo $PRTag
 echo "~Prueba Falsa~"
 branchName="[Test]UnaRama"
 echo $branchName
 PRTag=$(echo $branchName | cut -d '[' -f2 | cut -d ']' -f1)
-echo PRTag
+echo $PRTag
