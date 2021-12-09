602bff7f92cf892f3415b7f618a4da3f7b5038a2
Bruno Gutierrez <bruno.gutierrez96@gmail.com>
2021-12-09 17:46:29 -0500

    Deleting none[Prod]

--- .circleci/bin/check_and_freeze.sh
+++ .circleci/bin/check_and_freeze.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-branchName= "none"
+
 branchName= $(git log -1 --pretty=%B)
 echo $branchName
 PRTag=$(echo $branchName | cut -d '[' -f2 | cut -d ']' -f1)
 echo $PRTag
+
 echo "~Prueba Falsa~"
 branchName="[Test]UnaRama"
 echo $branchName
