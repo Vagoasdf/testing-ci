d32242aa48c8e41d0be707677d2bf32f5a9ab3ef
Bruno Gutierrez <bruno.gutierrez96@gmail.com>
2021-12-09 17:41:37 -0500

    Adding  Comands to capture the PR tag Name [Testing this]

---
+++ .circleci/bin/check_and_freeze.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+branchName= "none"
+branchName= $(git log -1 --pretty=%B)
+echo $branchName
+PRTag=$(echo $branchName | cut -d '[' -f2 | cut -d ']' -f1)
+echo PRTag
+echo "~Prueba Falsa~"
+branchName="[Test]UnaRama"
+echo $branchName
+PRTag=$(echo $branchName | cut -d '[' -f2 | cut -d ']' -f1)
+echo PRTag
