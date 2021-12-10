6fa46cb647bdc2d5422054a5a3874d76d4ff4853
Bruno Gutierrez <bruno.gutierrez96@gmail.com>
2021-12-10 15:23:42 -0500

    Adding the Key! [No Promote]

--- .circleci/bin/check_and_freeze.sh
+++ .circleci/bin/check_and_freeze.sh
@@ -1,22 +1,15 @@
 #!/usr/bin/env bash
 
-noPromote=$(cat is-no-promote)
-echo $noPromote
+noPromote=true
+
 if [ $noPromote ]; then
-  echo "$CIRCLE_REPOSITORY_URL"
-  echo "$CIRCLE_PROJECT_REPONAME"
-  echo "$CIRCLE_PROJECT_USERNAME"
-  echo "$CIRCLE_BRANCH"
-  baseUrl="  https://www.mergefreeze.com/api/branches/"
-  finalURL= "$baseUrl$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/$CIRCLE_BRANCH/?access_token=[Access token]"
-  echo "Freeze the merge"
-  echo "$finalURL"
 
-  staticUsername="Medology"
-  staticReponame="stdcheck.com"
-  staticBranchname="master"
-  staticURL= "$baseUrl$staticUsername/$staticReponame/$staticBranchname/?access_token=[Access token]"
-  echo "$staticURL"
+  baseUrl="https://www.mergefreeze.com/api/branches/"
+  ##Format: https://www.mergefreeze.com/api/branches/[Github account name]/[Github repository name]/[protected branch name]/?access_token=[Access token]
 
-  curl --data "frozen=true & user_name=circleci $finalURL"
+  finalURL="$baseUrl$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/$CIRCLE_BRANCH/?access_token=$GITHUB_TOKEN"
+
+  echo "Merge Freeze in action"
+  echo $finalURL
+  curl --data "frozen=true & user_name=circleci" "$finalURL"
 fi
