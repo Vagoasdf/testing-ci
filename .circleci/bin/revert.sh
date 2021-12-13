5122fa924dbeca5977dfa5fb6ce48fa78a106611
Bruno Gutierrez <bruno.gutierrez96@gmail.com>
2021-12-13 12:49:22 -0500

    trying a full revert flow

--- .circleci/bin/revert.sh
+++ .circleci/bin/revert.sh
@@ -4,12 +4,9 @@
 
 ## Revert the previous merge
 currentCommit="$(git rev-parse CIRCLE_BRANCH)"
-git revert "$currentCommit"
-git commit -m"[ci skip] Reverting the  No Promote Merge" --allow-empty
+git revert "$currentCommit" ## Check the merge stuf
 ## DO NOT TRIGGER ANOTHER CircleCI Pipeline.
-## add [ci skip] to the commit
-
+git commit -m"[ci skip] Reverting the  No Promote Merge" --allow-empty
 ## Push the unmerged head to  master branch
-
-
+git push
 #### Edge Case >> If a bot pushes something on top.
