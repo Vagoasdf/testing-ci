83472aa152c68a0edd01ba74fd38738f8836f6f3
Bruno Gutierrez <bruno.gutierrez96@gmail.com>
2021-12-13 14:28:24 -0500

    Adding the Whole Git Deal

--- .circleci/bin/revert.sh
+++ .circleci/bin/revert.sh
@@ -3,7 +3,7 @@
 ## 1. Stop the current Gcloud Version, "DONT DELETE IT"
 
 ## Revert the previous merge
-currentCommit="$(git rev-parse CIRCLE_BRANCH)"
+currentCommit="$(git rev-parse $CIRCLE_BRANCH)"
 git revert "$currentCommit" ## Check the merge stuf
 ## DO NOT TRIGGER ANOTHER CircleCI Pipeline.
 git commit -m"[ci skip] Reverting the  No Promote Merge" --allow-empty
