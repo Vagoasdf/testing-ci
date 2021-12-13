3bd4d68c99214778a3687541f94419e6178acbd0
Bruno Gutierrez <bruno.gutierrez96@gmail.com>
2021-12-13 15:32:09 -0500

    Revert "Revert "Merge pull request #3 from Vagoasdf/BuildupWorkflow""
    This reverts commit 9f7914a11df16adb24589e3647cbaf18ac45f4a8.

---
+++ .circleci/bin/revert.sh
@@ -0,0 +1,13 @@
+## This is gonna be hard.
+
+## 1. Stop the current Gcloud Version, "DONT DELETE IT"
+
+## Revert the previous merge
+currentCommit="$(git rev-parse )"
+git revert "$currentCommit" ## Check the merge stuf
+## DO NOT TRIGGER ANOTHER CircleCI Pipeline.
+git commit -m"[ci skip] Reverting the  No Promote Merge" --allow-empty
+## Push the unmerged head to  master branch
+git push --set-upstream origin "$CIRCLE_BRANCH"
+
+#### Edge Case >> If a bot pushes something on top.
