30936582e17894d5596b03c1818b2040fbc772d2
Bruno Gutierrez <bruno.gutierrez96@gmail.com>
2021-12-13 15:57:23 -0500

    Adding Good git revert nicities, and checking the base of the workflow

--- .circleci/bin/revert.sh
+++ .circleci/bin/revert.sh
@@ -3,10 +3,10 @@
 ## 1. Stop the current Gcloud Version, "DONT DELETE IT"
 
 ## Revert the previous merge
-git revert HEAD -m1 ##  M1  = Continue on main branch
-## DO NOT TRIGGER ANOTHER CircleCI Pipeline.
+git revert -n HEAD -m1 ##  M1  = Continue on main branch
 git commit -m"[ci skip] Reverting the  No Promote Merge" --allow-empty
 ## Push the unmerged head to  master branch
 git push --set-upstream origin "$CIRCLE_BRANCH"
 
 #### Edge Case >> If a bot pushes something on top.
+"
